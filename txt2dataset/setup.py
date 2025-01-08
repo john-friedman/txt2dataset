cf480dde5baa67c06f832477b5f26600babdab7a
John <jgfriedman99@gmail.com>
2025-01-07 23:18:35 -0800

    test

--- txt2dataset/setup.py
+++ txt2dataset/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="txt2dataset",
-    version="0.01",
+    version="0.02",
     packages=find_packages(),
     install_requires=[
         "pandas",
