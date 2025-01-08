b9c176f11a32c483fb7e7fac01bafe5bff616ce8
John <jgfriedman99@gmail.com>
2025-01-07 17:35:12 -0800

    test

--- datasetbuilder/setup.py
+++ datasetbuilder/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="datasetbuilder",
+    name="txt2dataset",
     version="0.01",
     packages=find_packages(),
     install_requires=[
